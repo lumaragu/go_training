f52b813e3f73c912d7dc132e53a3101c934bf63e
Luis Ramirez <luis.ramirez@thebeat.co>
2022-06-30 17:13:14 -0500

    Adding docker composer to the service

--- chapter8/db/mysql.go
+++ chapter8/db/mysql.go
@@ -8,9 +8,9 @@ import (
 
 func ConnectDB() *sql.DB {
 	dbDriver := "mysql"
-	dbUser := "root"
-	dbPass := "123456"
-	dbName := "testing"
+	dbUser := "user"
+	dbPass := "pass"
+	dbName := "db"
 	dbURI := "(127.0.0.1:3306)"
 	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp"+dbURI+"/"+dbName+"?parseTime=true")
 	if err != nil {
